c12691f1b58d16161f84c932991be4604304fc5d
Pedro Lucas R S Campos <plucasrsc@gmail.com>
2023-06-22 12:47:12 -0300

    Refatoração classe Explosion
    Mudança em nome de variável e redução de parâmetros pra iniciliazação
    (toda explosão terá massa próxima de 0 e velocidade inicial 0)

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -4,12 +4,16 @@ import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
 class Explosion(
-  initialPosition: Point2D,
-  initialVelocity: Vector2D,
-  radius: Double,
-  mass: Double,
+    initialPosition: Point2D,
+    radius: Double,
 ) :
-  SpaceObject("Explosion", '*', initialPosition, initialVelocity, radius, mass) {
-  var is_triggered = false
-
-  }
+    SpaceObject(
+        "Explosion",
+        '*',
+        initialPosition,
+        initialVelocity = Vector2D(0.0, 0.0),
+        radius,
+        mass = 1e-10
+    ) {
+  var has_finnished = false
+}
