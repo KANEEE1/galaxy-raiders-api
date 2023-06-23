e76a9bdd43830186ad6a3305420e2b86e426ad4e
Pedro Lucas R S Campos <plucasrsc@gmail.com>
2023-06-23 11:09:36 -0300

    Correção Leaderboard e Atualização Scoreboard
    Comportamento passado:
    - Caso o jogo atual entrasse pra 2º melhor pontuação da Leaderboard,
    a 3º melhor pontuação era sobreescrita pelo jogo atual
    
    Correção:
    - Modificação na função add, para considerar qual índice remover (previousIndexOfSameGame)
    
    - Também adicionamos em Scoreboard.json um jogo onde não há asteróides
    destruídos, mas não adicionamos em Leaderboard.json.

--- app/src/main/kotlin/galaxyraiders/core/score/BoardFile.kt
+++ app/src/main/kotlin/galaxyraiders/core/score/BoardFile.kt
@@ -10,9 +10,13 @@ abstract class BoardFile(private val fileName: String) {
 
     data class Board(var games: MutableList<GameScore>)
 
-    val board = this.read()
+    val board: Board
 
-    var useLastGame = false
+    init {
+        board = this.read()
+        this.firstTimeAdd(GameScore(0.0, 0))
+        this.write()
+    }
 
     private fun read(): Board {
         val file = File(fileName)
@@ -22,6 +26,8 @@ abstract class BoardFile(private val fileName: String) {
         val jsonText = file.readText(Charsets.UTF_8)
         return Klaxon().parse<Board>(jsonText)!!
     }
+
+    abstract fun firstTimeAdd(gameScore: GameScore)
 
     abstract fun add(gameScore: GameScore)
 
@@ -39,30 +45,52 @@ abstract class BoardFile(private val fileName: String) {
     }
 
     fun update(gameScore: GameScore) {
-        if (useLastGame) {
-            this.board.games.removeLast()
-        } else {
-            useLastGame = true
-        }
         this.add(gameScore)
         this.write()
     }
 }
 
 class ScoreboardFile:
-    BoardFile("./src/main/kotlin/galaxyraiders/core/score/Scoreboard.json") {
+    BoardFile("./src/main/kotlin/Scoreboard.json") {
+
+    override fun firstTimeAdd(gameScore: GameScore) {
+        this.board.games.add(gameScore)
+    }
+
     override fun add(gameScore: GameScore) {
+        this.board.games.removeLast()
         this.board.games.add(gameScore)
     }
 }
 
 class LeaderboardFile:
-    BoardFile("./src/main/kotlin/galaxyraiders/core/score/Leaderboard.json") {
+    BoardFile("./src/main/kotlin/Leaderboard.json") {
+    private var previousIndexOfSameGame = -1
+
+    override fun firstTimeAdd(gameScore: GameScore) {
+        /* Won't put in the leaderboard games with 0 asteroids destroyed */
+    }
+
     override fun add(gameScore: GameScore) {
-        this.board.games.add(gameScore)
-        this.board.games.sortByDescending { it.finalScore }
-        if (this.board.games.size > 3) {
-            this.board.games.removeLast()
+        if (previousIndexOfSameGame != -1) {
+            this.board.games.removeAt(previousIndexOfSameGame)
         }
+        val i = indexToAdd(gameScore)
+        previousIndexOfSameGame = i
+        if (i != -1) {
+            this.board.games.add(i, gameScore)
+        }
+    }
+
+    private fun indexToAdd(gameScore: GameScore): Int {
+        var i = 0
+        while (i < this.board.games.size &&
+                gameScore.finalScore < this.board.games.elementAt(i).finalScore) {
+            i++
+        }
+        if (i == 3) {
+            return -1
+        }
+        return i
     }
 }
